a32a3726d0c9ed84ac993a14ea382450ee75e7db
Rodrigo Weigert <rodrigo.neorooker@gmail.com>
2016-04-15 11:47:48 -0300

    New way of gettiing mimetypes.

--- webserver.py
+++ webserver.py
@@ -1,7 +1,8 @@
 import socket
 import datetime
 import commands 
-import os
+#import os
+import mimetypes
 
 class HTTPResponse:
 	__reasonPhrase = {
@@ -61,13 +62,19 @@ while 1:
 		connectionSocket.close()
 		continue
 
+	if ("." not in filename):
+		
+
 	try:
 		f = open(filename, "r")
 	except (IOError):
 		response = HTTPResponse(404, "close", "text/html", "")
 		connectionSocket.send(response.getMessage())
 	else:
-		mimetype = commands.getstatusoutput("file -b --mime-type " + filename)[1]
+		(mimetype, _) = mimetypes.guess_type(filename)
+		#mimetype = commands.getstatusoutput("file -b --mime-type " + filename)[1]
+		#if (".css" in filename):
+		#mimetype = "text/css"
 		content = f.read()
 		response = HTTPResponse(200, "close", mimetype, content)
 		connectionSocket.send(response.getMessage())
